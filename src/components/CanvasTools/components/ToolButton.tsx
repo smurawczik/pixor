8e9d99d97a90805137650116c1749841be271fd3
smurawczik <sebastian.murawczik@gmail.com>
2022-07-27 23:03:19 +0200

    dont forward isSelected to react compoennt

--- src/components/CanvasTools/components/ToolButton.tsx
+++ src/components/CanvasTools/components/ToolButton.tsx
@@ -1,7 +1,9 @@
 import { IconButton } from "@mui/material";
 import styled from "styled-components";
 
-export const ToolButton = styled(IconButton)<{ isSelected: boolean }>`
+export const ToolButton = styled(IconButton).withConfig({
+  shouldForwardProp: (prop) => !['isSelected'].includes(prop),
+})<{ isSelected: boolean }>`
   && {
     box-shadow: 0 0 0 1px
       ${({ isSelected, theme }) =>
