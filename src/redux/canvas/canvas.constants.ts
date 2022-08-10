2229898f509dbe87ce6844ae266d0219eadc35f8
smurawczik <sebastian.murawczik@gmail.com>
2022-08-10 19:06:15 +0200

    scale faftor

--- src/redux/canvas/canvas.constants.ts
+++ src/redux/canvas/canvas.constants.ts
@@ -1,5 +1,5 @@
 export const CANVAS_INITIAL_WIDTH = 30;
 export const CANVAS_INITIAL_HEIGHT = 30;
 export const CANVAS_DIMENSION_MULTIPLIER = window.devicePixelRatio || 1;
-export const CANVAS_SCALE_FACTOR = 4;
+export const CANVAS_SCALE_FACTOR = 5;
 export const CANVAS_TRANSPARENT_COLOR = "-";
